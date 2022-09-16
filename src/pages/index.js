e2a58a27ce9fd7ed47243f5f914df060f68c3a17
Mariam Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2022-09-16 09:58:09 -0500

    initial commit 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <StickyProvider>
         <Layout>
-          <SEO title="Startup Landing 006" />
+          <SEO title="Eduverso" />
           <Banner />
           <BoostAgencies />
           
